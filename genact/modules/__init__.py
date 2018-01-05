fa8bb2c04ed00c8b33e6a8805e74711d83ec8b25
Sven-Hendrik Haase <svenstaro@gmail.com>
2018-01-05 11:53:02 +0100

    Start rust rewrite

--- genact/modules/__init__.py
+++
@@ -1,7 +0,0 @@
-import genact.modules.bootlog
-import genact.modules.bruteforce
-import genact.modules.download
-import genact.modules.dump
-import genact.modules.initialize
-import genact.modules.botnet
-import genact.modules.heartbeat
