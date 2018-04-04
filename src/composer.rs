b196ddab95168ec7daf67686b1699258dea04acf
TheIronBorn <danielwatson311@gmail.com>
2018-04-04 22:23:23 +0000

    switch package-gen from hashmap to sample_slice

--- src/composer.rs
+++ src/composer.rs
@@ -1,36 +1,32 @@
 /// Module that pretends to install composer packages.
 use rand::{thread_rng, Rng, ThreadRng};
 use rand::distributions::{ChiSquared, IndependentSample};
-use std::collections::HashMap;
+use rand::seq::sample_slice;
 use yansi::Paint;
 
 use utils::csleep;
 use COMPOSERS_LIST;
 use parse_args::AppConfig;
 
-fn gen_package(packages: &[&str], mut rng: &mut ThreadRng) -> (String, String) {
+fn gen_package_version(rng: &mut ThreadRng) -> String {
     let chi = ChiSquared::new(1.0);
-    let package_version = format!(
+    format!(
         "{major:.0}.{minor:.0}.{patch:.0}",
-        major = 10.0 * chi.ind_sample(&mut rng),
-        minor = 10.0 * chi.ind_sample(&mut rng),
-        patch = 10.0 * chi.ind_sample(&mut rng)
-    );
-    let package_name = rng.choose(packages).unwrap_or(&"");
-    (package_name.to_string(), package_version.to_string())
+        major = 10.0 * chi.ind_sample(rng),
+        minor = 10.0 * chi.ind_sample(rng),
+        patch = 10.0 * chi.ind_sample(rng)
+    )
 }
 
 pub fn run(appconfig: &AppConfig) {
     let mut rng = thread_rng();
     let num_packages = rng.gen_range(10, 100);
-    let mut chosen_packages = HashMap::new();
-
-    while chosen_packages.len() < num_packages {
-        let (package_name, package_version) = gen_package(&COMPOSERS_LIST, &mut rng);
-        chosen_packages
-            .entry(package_name)
-            .or_insert(package_version);
-    }
+    // Choose `num_packages` packages, non-repeating and in random order
+    let chosen_names = sample_slice(&mut rng, &COMPOSERS_LIST, num_packages);
+    let chosen_packages: Vec<_> = chosen_names
+        .iter()
+        .map(|name| (name, gen_package_version(&mut rng)))
+        .collect();
 
     println!(
         "{text}",
