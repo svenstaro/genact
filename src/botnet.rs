b7eadcdbf33fdd568d0fac824eeeb9ed2b49135e
equal-l2 <eng.equall2@gmail.com>
2018-06-01 17:01:52 +0900

    Translate botnet to Rust

---
+++ src/botnet.rs
@@ -0,0 +1,73 @@
+use parse_args::AppConfig;
+use rand::{thread_rng, Rng};
+use utils::{csleep, dprint};
+use yansi::Paint;
+
+pub fn run(appconfig: &AppConfig) {
+    let mut rng = thread_rng();
+    let clusters = {
+        let mut ret = vec![];
+        for _ in 0..rng.gen_range(8, 16 + 1) {
+            ret.push(rng.gen_range(100, 200 + 1));
+        }
+        ret
+    };
+
+    let mut onlines = vec![false; clusters.len()];
+    let size: usize = clusters.iter().sum();
+
+    let mut connected = 0;
+
+    while connected <= size {
+        print!("\rEstablishing connections: {:4}/{:4}", connected, size);
+        connected += 1;
+        csleep((rng.gen_range(0_f64, 1.).powi(50) * 50.) as u64);
+    }
+    println!();
+
+    csleep(300);
+
+    for (i, nodes) in clusters.iter().enumerate() {
+        dprint(format!("  Cluster #{:02} ({:3} nodes)\n", i, nodes), 10);
+        csleep(100);
+    }
+
+    loop {
+        print!("\u{001b}[{}A\n", onlines.len() + 1);
+        for (i, (nodes, online)) in clusters.iter().zip(onlines.iter()).enumerate() {
+            // cannot use bright color because yansi doesn't support it
+            println!(
+                "\u{001b}[2K  Cluster #{:02} ({:3} nodes) [{}]",
+                i,
+                nodes,
+                if *online {
+                    Paint::green("online")
+                } else {
+                    Paint::yellow("booting")
+                },
+            );
+        }
+        if onlines.iter().all(|x| *x) {
+            break;
+        }
+        for o in &mut onlines {
+            if rng.gen_range(0., 1.) > 0.95_f64 {
+                *o = true;
+            }
+        }
+        csleep(100);
+    }
+
+    for task in &[
+        "Synchronizing clocks...",
+        "Sending login information...",
+        "Sending command...",
+    ] {
+        csleep(300);
+        dprint(format!("+ {} ", task), 10);
+        csleep(600);
+        dprint("[done]\n", 10)
+    }
+
+    dprint(">> Botnet update complete.\n", 10)
+}
