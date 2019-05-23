09a4a48f0dc97a156c371aee1740ab28576ab025
Kovah <mail@kovah.de>
2019-05-23 11:07:14 +0200

    Fix last small issues

--- src/docker.rs
+++ src/docker.rs
@@ -15,39 +15,37 @@ pub fn run(appconfig: &AppConfig) {
     // Choose `num_packages` packages, non-repeating and in random order
     let chosen_packages: Vec<_> = DOCKER_PACKAGES_LIST.choose_multiple(&mut rng, num_packages).collect();
 
-    for _stage in &["Deleting"] {
-        for &package_name in &chosen_packages {
-            let sleep_length = rng.gen_range(500, 5000);
-            let package_tag: &&str = DOCKER_TAGS_LIST.choose(&mut rng).unwrap();
+    for &package_name in &chosen_packages {
+        let sleep_length = rng.gen_range(500, 5000);
+        let package_tag: &&str = DOCKER_TAGS_LIST.choose(&mut rng).unwrap();
 
+        println!(
+            "Untagged: {package_name}:{package_tag}",
+            package_name = package_name,
+            package_tag = package_tag,
+        );
+        println!(
+            "Untagged: {package_name}:{package_tag}@sha256:{hash}",
+            package_name = package_name,
+            package_tag = package_tag,
+            hash = gen_hex_string(&mut rng, 64)
+        );
+
+        let num_hashes = rng.gen_range(10, 30);
+        let mut index = 0;
+        while index < num_hashes {
             println!(
-                "Untagged: {package_name}:{package_tag}",
-                package_name = package_name,
-                package_tag = package_tag,
-            );
-            println!(
-                "Untagged: {package_name}:{package_tag}@sha256:{hash}",
-                package_name = package_name,
-                package_tag = package_tag,
+                "Deleted: sha256:{hash}", 
                 hash = gen_hex_string(&mut rng, 64)
             );
 
-            let num_hashes = rng.gen_range(10, 30);
-            let mut index = 0;
-            while index < num_hashes {
-                println!(
-                    "Deleted: sha256:{hash}", 
-                    hash = gen_hex_string(&mut rng, 64)
-                );
+            index += 1;
+        }
 
-                index = index + 1;
-            }
+        csleep(sleep_length);
 
-            csleep(sleep_length);
-
-            if appconfig.should_exit() {
-                return;
-            }
+        if appconfig.should_exit() {
+            return;
         }
     }
 }
