6be80f2fef3277645fc66435a8f331e32a151071
Kovah <mail@kovah.de>
2020-02-24 17:42:03 +0100

    Implement Docker build using the output of other modules

--- src/docker_build.rs
+++ src/docker_build.rs
@@ -1,11 +1,28 @@
 /// Module that pretends to build Docker images
 use rand::prelude::*;
 use rand::Rng;
+use humantime::Duration;
+
 use crate::utils::{csleep, dprint, gen_hex_string};
 use crate::DOCKER_PACKAGES_LIST;
 use crate::DOCKER_TAGS_LIST;
 use crate::parse_args::AppConfig;
+use crate::parse_args::parse_args;
 
+use crate::bootlog;
+use crate::botnet;
+use crate::cargo;
+use crate::cc;
+use crate::composer;
+use crate::cryptomining;
+use crate::docker;
+use crate::docker_build;
+use crate::download;
+use crate::kernel_compile;
+use crate::memdump;
+use crate::mkinitcpio;
+use crate::simcity;
+use crate::weblog;
 
 pub fn run(appconfig: &AppConfig) {
     let mut rng = thread_rng();
@@ -33,7 +50,7 @@ pub fn run(appconfig: &AppConfig) {
         if appconfig.should_exit() {
             return;
         }
-        
+
         csleep(200);
     }
 
@@ -43,14 +60,14 @@ pub fn run(appconfig: &AppConfig) {
 
     while current_step <= total_steps {
         // Choose a random instruction
-        let instruction = "COPY ./config /app/config";
+        let command = select_command();
 
         // Print the current step with the instruction to run
         println!(
-            "Step {current_step}/{total_steps} : {instruction}",
+            "\rStep {current_step}/{total_steps} : {instruction}",
             current_step = current_step,
             total_steps = total_steps,
-            instruction = instruction
+            instruction = get_signature(command)
         );
 
         if rand::random() {
@@ -58,7 +75,30 @@ pub fn run(appconfig: &AppConfig) {
                 " ---> Using cache"
             );
         } else {
-            // Print the instruction output here
+            println!(
+                " ---> Running in {step_hash}",
+                step_hash = gen_hex_string(&mut rng, 12),
+            );
+
+            let docker_appconfig = appconfig;
+            match command {
+                "docker_build_copy" => docker_build_copy(),
+                "bootlog" => bootlog::run(&docker_appconfig),
+                "botnet" => botnet::run(&docker_appconfig),
+                "cargo" => cargo::run(&docker_appconfig),
+                "cryptomining" => cryptomining::run(&docker_appconfig),
+                "simcity" => simcity::run(&docker_appconfig),
+                "mkinitcpio" => mkinitcpio::run(&docker_appconfig),
+                "cc" => cc::run(&docker_appconfig),
+                "download" => download::run(&docker_appconfig),
+                // "docker" => docker::run(&docker_appconfig),
+                // "docker_build" => docker_build::run(&docker_appconfig),
+                "memdump" => memdump::run(&docker_appconfig),
+                "composer" => composer::run(&docker_appconfig),
+                "kernel_compile" => kernel_compile::run(&docker_appconfig),
+                "weblog" => weblog::run(&docker_appconfig),
+                _ => panic!("Unknown module!"),
+            }
         }
 
         println!(
@@ -69,7 +109,7 @@ pub fn run(appconfig: &AppConfig) {
         if appconfig.should_exit() {
             return;
         }
-        
+
         current_step += 1;
         csleep(rng.gen_range(300, 1000));
     }
@@ -93,4 +133,54 @@ pub fn run(appconfig: &AppConfig) {
     if appconfig.should_exit() {
         return;
     }
+}
+
+fn select_command() -> &'static str {
+    let mut rng = thread_rng();
+
+    let available_modules = [
+        "docker_build_copy",
+        "bootlog",
+        "botnet",
+        "cargo",
+        "cc",
+        "composer",
+        "cryptomining",
+        "simcity",
+        "download",
+        // "docker",
+        // "docker_build",
+        "memdump",
+        "mkinitcpio",
+        "kernel_compile",
+        "weblog",
+    ];
+
+    let rand_choice: &str = available_modules.choose(&mut rng).unwrap();
+    return rand_choice;
+}
+
+fn get_signature(choice: &str) -> &str {
+    return match choice {
+        "docker_build_copy" => "COPY ./config /app/config",
+        "bootlog" => "RUN ",
+        "botnet" => "RUN ",
+        "cargo" => "RUN ",
+        "cryptomining" => "RUN ",
+        "simcity" => "RUN ",
+        "mkinitcpio" => "RUN ",
+        "cc" => "RUN ",
+        "download" => "RUN ",
+        // "docker" => "RUN ",
+        // "docker_build" => "RUN ",
+        "memdump" => "RUN ",
+        "composer" => "RUN ",
+        "kernel_compile" => "RUN ",
+        "weblog" => "RUN ",
+        _ => panic!("Unknown module!"),
+    }
+}
+
+fn docker_build_copy() {
+    return
 }
